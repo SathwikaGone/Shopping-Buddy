d2b0eac51fc62403793770f9d3ced247c5b55aa2
Susritha <susrithagade1996@>
2019-06-19 14:44:13 -0500

    commiting home screen

--- Code/ShoppingBuddy/app/src/main/java/com/example/shoppingbuddy/Home.java
+++ Code/ShoppingBuddy/app/src/main/java/com/example/shoppingbuddy/Home.java
@@ -1,10 +1,8 @@
 package com.example.shoppingbuddy;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,18 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
-public class SigninActivity extends AppCompatActivity
+public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    public Button btn;
-    public Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
-        Intent i=getIntent();
+        setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -39,24 +33,7 @@ public class SigninActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        btn=(Button)findViewById(R.id.button3);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(SigninActivity.this,MainActivity.class);
-                Log.d("click","button clicked");
-                startActivity(i);
-            }
-        });
-        btn1=(Button)findViewById(R.id.button2);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(SigninActivity.this,Home.class);
-                Log.d("click","button clicked");
-                startActivity(i);
-            }
-        });
+
     }
 
     @Override
@@ -72,7 +49,7 @@ public class SigninActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.signin, menu);
+        getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
