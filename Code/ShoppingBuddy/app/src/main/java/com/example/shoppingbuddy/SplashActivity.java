4e47fabbcecd3c8e0b19315c2a9ca6672ad78cb6
Susritha <susrithagade1996@>
2019-06-24 14:01:09 -0500

    commiting Splash activity

---
+++ Code/ShoppingBuddy/app/src/main/java/com/example/shoppingbuddy/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.shoppingbuddy;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        SystemClock.sleep(3000);
+        Intent login = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(login);
+        finish();
+    }
+}
