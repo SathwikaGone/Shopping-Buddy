8a7740a8bb87fe6feb5543b31799bb728d82a3c9
vennela-cheekoti <s533619@NWMISSOURI.EDU>
2019-09-14 21:12:59 -0500

    committing code for add product

--- Code/ShoppingBuddy/app/src/main/java/com/example/shoppingbuddy/AddProductActivity.java
+++ Code/ShoppingBuddy/app/src/main/java/com/example/shoppingbuddy/AddProductActivity.java
@@ -1,15 +1,30 @@
 package com.example.shoppingbuddy;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.storage.StorageManager;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -18,17 +33,116 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.MimeTypeMap;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddProductActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private Spinner spinner;
+    private static final String[] paths = {"Men's clothing", "Women's clothing", "Kid's clothing",
+            "Men's Accessories", "Women's Accessories", "Cell Phones and Accessories",
+            "Tv and Computers", "Men's Footwear", "Women's Footwear","Books","Sports Equipment","Hair","Skin","Makeup"};
+    private FirebaseFirestore db;
+    private StorageReference storage, storg;
+    private CollectionReference itemCollection;
+    private DocumentReference itemDoc;
+    private static final int PICK_IMG_REQUEST = 1;
+    Button uploadbtn;
+    private Uri imageURI;
+    private Long imageName;
+    private String imageURL;
+    ImageView prodimage;
+    String id,name,description, category;
+    Double cost;
+    EditText productid,productname,productdes,productcost;
+    Spinner productcate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+         productid=findViewById(R.id.editText6);
+         productname=findViewById(R.id.editText9);
+         productdes=findViewById(R.id.editText12);
+         productcate=findViewById(R.id.spinner);
+         productcost=findViewById(R.id.editText13);
+        prodimage=findViewById(R.id.imageView3);
+        uploadbtn=findViewById(R.id.button13);
+        Button addprod=findViewById(R.id.button12);
+
+
+
+        storage = FirebaseStorage.getInstance().getReference("productimages");
+        storg = FirebaseStorage.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
+        itemCollection = db.collection("products");
+
+
+
+        addprod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id=productid.getText().toString();
+                name=productname.getText().toString();
+                description=productdes.getText().toString();
+                category=productcate.getSelectedItem().toString();
+                cost=Double.parseDouble(productcost.getText().toString());
+
+                if(id.equals("")||name.equals("")||description.equals("") || category.equals("") || imageURL.equals("") ||   cost<=0){
+                    Toast.makeText(AddProductActivity.this, "All the field needs to be filled ", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Map<String, Object> addproduct = new HashMap<>();
+                    addproduct.put("itemId", id);
+                    addproduct.put("itemName", name);
+                    addproduct.put("itemDetails", description);
+                    addproduct.put("category", category);
+                    addproduct.put("cost", cost);
+                    addproduct.put("imageURL", imageURL);
+
+                    itemCollection.document().set(addproduct);
+                    Toast.makeText(AddProductActivity.this, "Item added to the list", Toast.LENGTH_SHORT).show();
+
+                    finish();
+
+
+                }
+
+            }
+        });
+
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddProductActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
 
 
 
@@ -50,6 +164,7 @@ public class AddProductActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+    
 
     @Override
     public void onBackPressed() {
@@ -107,4 +222,5 @@ public class AddProductActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
